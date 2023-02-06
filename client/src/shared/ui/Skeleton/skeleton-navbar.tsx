2ef2673c150a5cc7f9dc967d1bffe00eb3247d9d
thesaddest <artsemshauchuk@gmail.com>
2023-02-06 12:04:30 +0100

    add: mobile layout for skeleton-navbar

--- client/src/shared/ui/Skeleton/skeleton-navbar.tsx
+++ client/src/shared/ui/Skeleton/skeleton-navbar.tsx
@@ -1,23 +1,36 @@
 import { FC } from "react";
-import { Skeleton, Space } from "antd";
+import { Skeleton } from "antd";
 import styled from "styled-components";
 
 const StyledNavbarContainer = styled.div`
     display: flex;
     align-items: center;
-    justify-content: center;
-    gap: 8px;
+    justify-content: start;
+    gap: 0.5rem;
     padding: 0.5rem 0 0 0.5rem;
+    width: 100%;
+
+    @media only screen and (max-width: 425px) {
+        justify-content: space-evenly;
+    }
+`;
+
+const StyledButtonsContainer = styled.div`
+    display: flex;
+    gap: 0.5rem;
+    padding-right: 2rem;
 `;
 
 export const SkeletonNavbar: FC = () => {
     return (
-        <Space>
-            <StyledNavbarContainer>
-                <Skeleton.Input active size="default" style={{ height: "28px", minWidth: "10px", width: "105px" }} />
-                <Skeleton.Button active style={{ minWidth: "42px", width: "46px" }} />
-                <Skeleton.Button active style={{ minWidth: "42px", width: "46px" }} />
-            </StyledNavbarContainer>
-        </Space>
+        <StyledNavbarContainer>
+            <div>
+                <Skeleton.Input active size="default" style={{ height: "26px", minWidth: "10px", width: "70px" }} />
+            </div>
+            <StyledButtonsContainer>
+                <Skeleton.Button active style={{ minWidth: "42px", width: "46px", height: "32px" }} />
+                <Skeleton.Button active style={{ minWidth: "42px", width: "46px", height: "32px" }} />
+            </StyledButtonsContainer>
+        </StyledNavbarContainer>
     );
 };
