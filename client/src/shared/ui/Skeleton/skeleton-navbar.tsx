54fdd7b2d5275b466851599d008ac1428198613e
thesaddest <artsemshauchuk@gmail.com>
2023-02-11 18:08:09 +0100

    fix: desktop skeleton chat layout

--- client/src/shared/ui/Skeleton/skeleton-navbar.tsx
+++ client/src/shared/ui/Skeleton/skeleton-navbar.tsx
@@ -19,14 +19,23 @@ const StyledButtonsContainer = styled.div`
     display: flex;
     gap: 0.5rem;
     padding-right: 2rem;
+
+    @media only screen and (min-width: 1024px) {
+        padding-left: 9rem;
+    }
+`;
+
+const StyledInputContainer = styled.div`
+    display: flex;
+    padding-left: 3rem;
 `;
 
 export const SkeletonNavbar: FC = () => {
     return (
         <StyledNavbarContainer>
-            <div>
+            <StyledInputContainer>
                 <Skeleton.Input active size="default" style={{ height: "26px", minWidth: "10px", width: "70px" }} />
-            </div>
+            </StyledInputContainer>
             <StyledButtonsContainer>
                 <Skeleton.Button active style={{ minWidth: "42px", width: "46px", height: "32px" }} />
                 <Skeleton.Button active style={{ minWidth: "42px", width: "46px", height: "32px" }} />
